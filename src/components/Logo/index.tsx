5907ac1f45fe655422bfd7a6e2d5285e2aaa6471
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2023-02-07 01:01:08 -0300

    feat: create logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import { GraduationCap } from 'phosphor-react'
+
+import { Container } from './styles'
+
+export function Logo() {
+  return (
+    <Container>
+      <GraduationCap size={32} weight="fill" color="#FFFFFF" />
+    </Container>
+  )
+}
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #003BAE;
+`
