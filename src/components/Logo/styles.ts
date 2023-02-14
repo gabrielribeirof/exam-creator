eecbac01f87312ce9a3eae3eb3f8eb4aeec603a7
Gabriel Ribeiro <39045611+gabrielribeirof@users.noreply.github.com>
2023-02-14 03:01:45 +0000

    feat: create logo component size constants

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
 
+export const LOGO_WIDTH_IN_PX = 50
+export const LOGO_HEIGHT_IN_PX = 50
+
 export const Container = styled.div`
-  width: 50px;
-  height: 50px;
+  width: ${LOGO_WIDTH_IN_PX}px;
+  min-width: ${LOGO_WIDTH_IN_PX}px;
+  height: ${LOGO_HEIGHT_IN_PX}px;
   border-radius: 16px;
 
   display: flex;
