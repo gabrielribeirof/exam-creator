b8d8ef9581813d6fa94f4755e71157d1c21514ea
Gabriel Ribeiro <39045611+gabrielribeirof@users.noreply.github.com>
2023-02-14 02:58:00 +0000

    feat: update globalstyle

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
+    font-family: 'Inter';
+    color: #202020;
   }
 `
