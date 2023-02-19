dd8b7f7f16cfa7ee619db3d8d4bec5557b3e6d84
Gabriel Ribeiro <39045611+gabrielribeirof@users.noreply.github.com>
2023-02-19 18:05:34 +0000

    feat: improve text global style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,6 +8,12 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     font-family: 'Inter';
+    font-size: 14px;
     color: #202020;
+  }
+
+  input, textarea, button {
+    font-family: 'Inter';
+    font-size: 14px;
   }
 `
