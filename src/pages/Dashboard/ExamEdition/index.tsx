781c836e43f506e5d4e4ad1a1a05c1da33d13ee5
Gabriel Ribeiro <39045611+gabrielribeirof@users.noreply.github.com>
2023-02-19 18:14:12 +0000

    feat: init exam edition page creation

---
+++ src/pages/Dashboard/ExamEdition/index.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Exam } from 'phosphor-react'
+
+import { DashboardBase } from '../../../components/DashboardBase'
+
+import { SettingStep } from './SettingStep'
+import { QuestionsStep } from './QuestionsStep'
+
+type STEPS = 'questions' | 'settings'
+
+export interface ExamSettings {
+  title: string
+  guidelines: string
+  classes: string[]
+  date: string
+  start_time: string
+  end_time: string
+}
+
+export function ExamEdition() {
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  const [step, setStep] = useState<STEPS>('settings')
+
+  useEffect(() => {
+    if (!id) alert('Exam not found')
+  }, [id])
+
+  return (
+    <DashboardBase
+      header={{
+        icon: Exam,
+        title: 'Exam Edition',
+        color: 'blue',
+        progressBar: [
+          {
+            name: 'Settings',
+            active: step === 'settings'
+          },
+          {
+            name: 'Questions',
+            active: step === 'questions'
+          }
+        ]
+      }}
+      content={{ smallerWidth: true }}
+    >
+      {step === 'settings' && <SettingStep onSubmitted={() => setStep('questions')} />}
+
+      {step === 'questions' && <QuestionsStep onSubmitted={() => navigate('/dashboard/exams')} />}
+    </DashboardBase>
+  )
+}
